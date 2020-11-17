266cea895c4712644eb15ebbec02d132e3355ce9
Student <cwalker1332@gmail.com>
2020-11-17 12:41:40 -0600

    changing name of class

--- src/DSAPractice/Maps/WordLen.java
+++ src/DSAPractice/Maps/WordLen.java
@@ -3,20 +3,20 @@ package DSAPractice.Maps;
 import java.util.Map;
 import java.util.HashMap;
 
-public class Maps {
+    /*
+        Exercise: Given an array of strings, return a Map<String, Integer> containing a
+        key for every different string in the array, and the value is that string's length.
+     */
+
+public class WordLen {
     public static void main(String[] args) {
         String[] array = {"a", "bb", "a", "bb"};
         System.out.println(wordLen(array));
     }
 
-    /*
-    Exercise: Given an array of strings, return a Map<String, Integer> containing a key for every different string
-    in the array, and the value is that string's length.
-     */
-
     public static Map<String, Integer> wordLen(String[] strings) {
         // create empty map
-        Map<String, Integer> map = new HashMap<String, Integer>();
+        Map<String, Integer> map = new HashMap<>();
 
         // for each string in strings, add string(key) and string length(value) to map
         for (String str : strings){
